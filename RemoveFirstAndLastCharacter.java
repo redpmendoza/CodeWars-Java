998b4955c364647c5408ff6cce514e0fd36a3d56
Rhabi Mendoza <rhabidpmendoza@gmail.com>
2023-09-01 21:29:36 +0800

    Create RemoveFirstAndLastCharacter.java

---
+++ RemoveFirstAndLastCharacter.java
@@ -0,0 +1,10 @@
+public class RemoveFirstAndLastCharacter{
+    public static String remove(String str){
+    
+      // Start of Solution
+      String removed = str.substring(1, (str.length()-1));
+      return removed; 
+      // End of Solution
+      
+    }
+}
