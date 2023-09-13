8cd22e4a51982ff76caa42233165bd99765152b7
Rhabi Mendoza <rhabidpmendoza@gmail.com>
2023-09-13 21:56:49 +0800

    Create ThirdAngleOfATriangle.java

---
+++ ThirdAngleOfATriangle.java
@@ -0,0 +1,9 @@
+public class ThirdAngleOfATriangle{
+    public static int otherAngle(int angle1, int angle2){
+      
+      // Start of Solution
+      return (180 - (angle1 + angle2));
+      // End of Solution
+
+    }
+}
