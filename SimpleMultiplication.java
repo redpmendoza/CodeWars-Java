36cc8a9cb0adb718d95a4745a0aea9c8436aaccd
Rhabi <rhabidpmendoza@gmail.com>
2023-09-18 22:18:51 +0800

    Create SimpleMultiplication.java

---
+++ SimpleMultiplication.java
@@ -0,0 +1,13 @@
+public class SimpleMultiplication{
+    public static int simpleMultiplication(int n){
+      
+      // Start of Solution
+      if(n % 2 != 0){
+        return n * 9;
+      }
+      
+      return n * 8;
+      // End of Solution
+
+    }
+}
