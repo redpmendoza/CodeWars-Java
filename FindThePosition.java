dbc662f78a28de00804d12b16413e0afa67a43b1
Rhabi <rhabidpmendoza@gmail.com>
2023-09-20 22:13:00 +0800

    Create FindThePosition.java

---
+++ FindThePosition.java
@@ -0,0 +1,11 @@
+public class FindThePosition{
+  public static String position(char alphabet){
+    
+    // Start of Solution
+    int num = (int) alphabet - 96;
+    
+    return "Position of alphabet: " + num;
+    // End of Solution
+    
+  }
+}
