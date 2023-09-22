905ee93370230709fa37c2c4fbe53cc0ecbdf224
Rhabi <rhabidpmendoza@gmail.com>
2023-09-22 22:12:43 +0800

    Create IfYouCantSleepJustCountSheep.java

---
+++ IfYouCantSleepJustCountSheep.java
@@ -0,0 +1,15 @@
+class IfYouCantSleepJustCountSheep{
+  public static String countingSheep(int num){
+    
+    // Start of Solution
+    String out = "";
+    
+    for(int ctr = 1; ctr <= num; ctr++){
+      out += ctr + " sheep...";
+    }
+    
+    return out;
+    // End of Solution
+    
+  }
+}
