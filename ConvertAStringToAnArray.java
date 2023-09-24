dac455880530456989b2c76df9eb7063671ce535
Rhabi <rhabidpmendoza@gmail.com>
2023-09-24 22:12:02 +0800

    Create ConvertAStringToAnArray.java

---
+++ ConvertAStringToAnArray.java
@@ -0,0 +1,10 @@
+public class ConvertAStringToAnArray{
+    public static String[] stringToArray(String s) {
+      
+      // Start of Solution
+      String arr[] = s.split(" ");
+      return arr;
+      // End of Solution
+      
+    }
+}
