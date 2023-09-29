98cc88ab6a4a5a389d25cff7fa145a19d0e21978
Rhabi <rhabidpmendoza@gmail.com>
2023-09-29 21:01:05 +0800

    Create TheWideMouthedFrog.java

---
+++ TheWideMouthedFrog.java
@@ -0,0 +1,13 @@
+public class TheWideMouthedFrog{
+  public static String mouthSize(String animal){
+   
+    // Start of Solution
+    if(animal.equalsIgnoreCase("alligator")){
+      return "small";
+    }
+    
+    return "wide";
+    // End of Solution
+    
+  }
+}
