0def9c4fb42a3af7a272c924d826648032d50e7c
Rhabi <rhabidpmendoza@gmail.com>
2023-10-12 20:36:22 +0800

    Create AreaOrPerimeter.java

---
+++ AreaOrPerimeter.java
@@ -0,0 +1,14 @@
+public class AreaOrPerimeter{
+    public static int areaOrPerimeter (int l, int w){
+       
+      // Start of Solution
+      if(l == w){
+        return l * w;
+      }
+      else{
+        return (2 * (l + w));
+      }
+      // End of Solution
+      
+    }
+}
