266b60f657b131a45ca91efa8f9e3cc31d0f7b87
Rhabi <rhabidpmendoza@gmail.com>
2023-10-15 22:21:54 +0800

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,9 @@
+public class EvenOrOdd{
+    public static String evenOrOdd(int number) {
+      
+      // Start of Solution
+      return number % 2 == 0 ? "Even" : "Odd";
+      // End of Solution
+      
+    }
+}
