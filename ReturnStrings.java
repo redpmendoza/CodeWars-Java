9521aaabe6970ccba754bb4ab54c9bfa9921edc0
Rhabi <rhabidpmendoza@gmail.com>
2023-10-18 18:45:03 +0800

    Create ReturnStrings.java

---
+++ ReturnStrings.java
@@ -0,0 +1,9 @@
+public class ReturnStrings{
+  public static String greet(String name){
+ 
+    // Start of Solution
+    return "Hello, " + name + " how are you doing today?";
+    // End of Solution
+    
+  }
+}
