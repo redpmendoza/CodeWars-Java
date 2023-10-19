0ab6eba3d2bd52eacc7aa2231a801262d62b8b15
Rhabi <rhabidpmendoza@gmail.com>
2023-10-19 21:58:38 +0800

    Create ReturnNegative.java

---
+++ ReturnNegative.java
@@ -0,0 +1,13 @@
+public class ReturnNegative{
+  public static int makeNegative(final int x){
+    
+    // Start of Solution
+    if(x > 1){
+      return x * -1;
+    }
+    
+    return x;
+    // End of Solution
+    
+  }
+}
