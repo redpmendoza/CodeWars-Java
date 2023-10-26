ed2fd2adc3fdb161010c87e417c4f281400d194e
Rhabi <rhabidpmendoza@gmail.com>
2023-10-26 20:59:23 +0800

    Create ConvertAStringToANumber.java

---
+++ ConvertAStringToANumber.java
@@ -0,0 +1,11 @@
+public class ConvertAStringToANumber{
+  public static int stringToNumber(String str){
+    
+    // Start of Solution
+    int num = Integer.parseInt(str);
+    
+    return num;
+    // End Solution
+    
+  }
+}
