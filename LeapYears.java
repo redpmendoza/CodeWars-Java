74d96ccc0897c425268a64a72a03079e223e5325
Rhabi <rhabidpmendoza@gmail.com>
2023-11-03 22:23:29 +0800

    Create LeapYears.java

---
+++ LeapYears.java
@@ -0,0 +1,17 @@
+public class LeapYears{
+  public static boolean isLeapYear(int year){
+    
+    // Start of Solution
+    if(year % 4 == 0 && year % 100 != 0){
+      return true;
+    }
+    
+    if(year % 4 == 0 && year % 400 == 0){
+      return true;
+    }
+    
+    return false;
+    // End of Solution
+    
+  }
+}
