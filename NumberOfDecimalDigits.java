df48e8da146027482bac529cf157f3b67cf0f684
Rhabi <rhabidpmendoza@gmail.com>
2023-11-06 21:46:19 +0800

    Create NumberOfDecimalDigits.java

---
+++ NumberOfDecimalDigits.java
@@ -0,0 +1,11 @@
+public class NumberOfDecimalDigits{
+  public static int Digits(long n){
+    
+    // Start of Solution
+    String num = n + "";
+    
+    return num.length();
+    // End of Solution
+
+  }
+}
