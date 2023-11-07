6d1c103c6833028b575162a185ee062534d6bff4
Rhabi <rhabidpmendoza@gmail.com>
2023-11-07 22:11:28 +0800

    Create SwitchItUp.java

---
+++ SwitchItUp.java
@@ -0,0 +1,44 @@
+public class SwitchItUp{
+  public static String switchItUp(int number){
+    
+    // Start of Solution
+    String num = "";
+    
+    switch(number){
+        case 0:
+          num = "Zero";
+        break;
+        case 1:
+          num = "One";
+        break;
+        case 2:
+          num = "Two";
+        break;
+        case 3:
+          num = "Three";
+        break;
+        case 4:
+          num = "Four";
+        break;
+        case 5:
+          num = "Five";
+        break;
+        case 6:
+          num = "Six";
+        break;
+        case 7:
+          num = "Seven";
+        break;
+        case 8:
+          num = "Eight";
+        break;
+        case 9:
+          num = "Nine";
+        break;
+    }
+    
+    return num;
+    // End of Solution
+    
+  }
+}
