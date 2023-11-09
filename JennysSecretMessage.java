d6a7c4165ee64f386ad42bef277269695afe9b88
Rhabi <rhabidpmendoza@gmail.com>
2023-11-09 21:32:25 +0800

    Create JennysSecretMessage.java

---
+++ JennysSecretMessage.java
@@ -0,0 +1,14 @@
+public class JennysSecretMessage{
+  public static String greet(String name){
+    
+    // Start of Solution
+    if(name.equals("Johnny")){
+      return "Hello, my love!";
+    }
+    else{
+      return "Hello, " + name + "!";
+    }
+    // End of Solution
+    
+  }
+}
