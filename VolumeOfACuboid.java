3b8a0d3ec70c8d8f2f82178fd27c6455bae6a699
Rhabi <rhabidpmendoza@gmail.com>
2023-11-21 21:47:11 +0800

    Create VolumeOfACuboid.java

---
+++ VolumeOfACuboid.java
@@ -0,0 +1,9 @@
+public class VolumeOfACuboid{
+  public static double getVolumeOfCuboid(final double length, final double width, final double height){
+    
+    // Start of Solution
+    return length * width * height;
+    // End of Solution
+    
+  }
+}
