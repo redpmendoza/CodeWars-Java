daccd7bc4f2efaba7251b3acbbf337e6754ab5f7
Rhabi <rhabidpmendoza@gmail.com>
2023-12-09 10:06:45 +0800

    Create GrasshopperSummation.java

---
+++ GrasshopperSummation.java
@@ -0,0 +1,15 @@
+public class GrasshopperSummation{
+  public static int summation(int n){
+    
+    // Start of Solution
+    int sum = 0;
+    
+    for(int ctr = 1; ctr <= n; ctr++){
+      sum += ctr;
+    }
+    
+    return sum;
+    // End of Solution
+    
+    }
+}
