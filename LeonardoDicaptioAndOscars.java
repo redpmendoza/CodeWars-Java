65f18796b4c789b5d5475821444c82536829ca1b
Rhabi <rhabidpmendoza@gmail.com>
2023-12-23 22:08:11 +0800

    Create LeonardoDicaptioAndOscars.java

---
+++ LeonardoDicaptioAndOscars.java
@@ -0,0 +1,20 @@
+public class LeonardoDicaptioAndOscars{
+  public static String leo(final int oscar){
+    
+    // Start of Solution
+    if(oscar == 88){
+      return "Leo finally won the oscar! Leo is happy";
+    }
+    else if(oscar == 86){
+      return "Not even for Wolf of wallstreet?!";
+    }
+    else if(oscar < 88){
+      return "When will you give Leo an Oscar?";
+    }
+    else{
+      return "Leo got one already!";
+    }
+    // End of Solution
+    
+  }
+}
