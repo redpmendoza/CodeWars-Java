8de5a4b18f8bf3e0d129e18eb5de9fa58f786a00
Rhabi <rhabidpmendoza@gmail.com>
2023-12-25 21:47:47 +0800

    Create FixMeHoursToSeconds.java

---
+++ FixMeHoursToSeconds.java
@@ -0,0 +1,10 @@
+public class FixMeHoursToSeconds{
+  public static long hoursToSeconds(final int hours){
+    
+    // Start of Solution
+    long out = (long) (hours * 60) * 60;
+    return out;
+    // End of Solution
+    
+  }
+}
